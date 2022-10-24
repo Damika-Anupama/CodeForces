025e663481e9c768fe6b6a95ff949ee76ed0724b
damika <damikaanupama@gmail.com>
2022-10-24 14:07:42 +0530

    Initial Commit

---
+++ Codeforces Global Round 23/1. Maxmina/Maxmina.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    freopen("Maxmina.txt","r",stdin);
+    int t,n,k;
+    scanf("%d",t);
+    for(int i = 0;i<t;i++)
+    {
+        scanf("%d %d",n,k);
+        
+    }
+    return 0;
+}
